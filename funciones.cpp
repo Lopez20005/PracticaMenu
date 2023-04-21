ba4f2d09c470076852bf81314a72a8fbb14a5ea3
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-21 08:29:49 -0600

    Se agrego funciones

--- funciones.cpp
+++ funciones.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 
 using namespace std ;
-#define MAX  10 
+
+#define MAX  100
 
 //arreglo 
 
@@ -10,6 +11,8 @@ int pos=0; // posocion de elemento
 
 void agregarEdad(int edad); 
 bool hayEspacio();
+void mostrarEdades();
+bool esMayor(int edad); 
 
 void agregarEdad(int edad ){
 
@@ -24,7 +27,31 @@ return pos < MAX ;
 
 }
 
+void mostrarEdadesMayores(){
 
+for (int i = 0; i < pos; i++ ){
+if(esMayor(edades[i]))
+cout << edades [i] << endl ; 
+} 
 
+}
 
+bool esMayor(int edad){
+
+return edad >=18; 
+
+}
+
+int menu (){
+    int opcion ; 
+cout << "Listado de edades " << endl ; 
+cout << "1. Agregar edad " << endl ; 
+cout << "2. Mostrar edades " << endl ; 
+cout << "3. Mostrar edades mayores o igual a 18 " << endl ; 
+cout << "4. Salir ";
+cout << "Digite la opcion "; 
+cin >> opcion ; 
+
+return 0 ; 
+}
 
